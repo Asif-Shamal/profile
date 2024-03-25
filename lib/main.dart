2c8bdfd4539bce32dca7da5559aefafac1219e51
Asif Shamal <asifshamal51@outlook.com>
2024-03-25 11:37:43 +0430

    profile

---
+++ lib/main.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(ProfileCard());
+}
+
+class ProfileCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: ProfileInfo(
+          name: 'M. Asif Shamal',
+          email: 'asifshamal51@outlook.com',
+          job: 'Software Engineer',
+          image: 'asset/img/scorpion.jpg', // Path to your image asset
+        ),
+      ),
+    );
+  }
+}
+
+class ProfileInfo extends StatelessWidget {
+  final String name;
+  final String email;
+  final String job;
+  final String image;
+
+  ProfileInfo({
+    required this.name,
+    required this.email,
+    required this.job,
+    required this.image,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [Colors.blue, Colors.blueAccent],
+        ),
+      ),
+      child: Center(
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Card(
+            elevation: 4,
+            child: SizedBox(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height,
+              child: Padding(
+                padding: EdgeInsets.all(16.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    CircleAvatar(
+                      radius: MediaQuery.of(context).size.width * 0.3,
+                      backgroundImage: AssetImage(image),
+                    ),
+                    SizedBox(height: 16),
+                    Text(
+                      name,
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(height: 8),
+                    Text(
+                      job,
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Colors.grey[600],
+                      ),
+                    ),
+                    SizedBox(height: 8),
+                    Text(
+                      email,
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Colors.grey[600],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
